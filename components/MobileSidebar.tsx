09cf15223a53c733e7f6e0e0c623cc6be01b8991
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-11-28 21:08:39 +0200

    created MobileSidebar component

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -1,14 +1,35 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 import { Menu } from "lucide-react";
 
 import { Button } from "./ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import Sidebar from "./Sidebar";
 
 const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
-    <Button variant="ghost" size="icon" className="md:hidden">
-      <Menu />
-    </Button>
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
   );
 };
 
