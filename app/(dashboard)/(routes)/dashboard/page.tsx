6c7337d6e2cdd875bf19e3c325324063f85d7d13
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-11-29 15:42:41 +0200

    added Card component from shadcn/ui | working on dashboardpage

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,31 @@
+"use client";
+
+import { MessageSquare } from "lucide-react";
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    href: "/conversation",
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+  },
+];
+
 const DashBoardPage = () => {
   return (
     <div>
-      <p>Dashboard page</p>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Exlore the power of AI
+        </h2>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          Chat with the smartest AI - Experience the power of AI
+        </p>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map(tool => )}
+      </div>
     </div>
   );
 };
