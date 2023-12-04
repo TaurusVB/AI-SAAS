f568a8fdefb47d7451e62e10e4d33273f8536342
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-12-04 18:38:17 +0200

    work on ProModal functionality

--- components/ProModal.tsx
+++ components/ProModal.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import useProModal from "@/hooks/useProModal";
+
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./ui/dialog";
 
 const ProModal = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const { isOpen, onClose } = useProModal();
+
+  return (
+    <Dialog open={true} onOpenChange={onClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
+            Upgrade to Genius
+          </DialogTitle>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default ProModal;
