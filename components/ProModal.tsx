a203a19777f4c420f6d479e9bd730fb81b71fba2
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-12-04 18:10:51 +0200

    created ModalProvider | created Pro modal

---
+++ components/ProModal.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProModal = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ProModal;
---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import ProModal from "@/components/ProModal";
+import { useEffect, useState } from "react";
+
+const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return <ProModal />;
+};
+
+export default ModalProvider;
